e671173f1163cc39e351ae390986c762820f441f
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-09 10:09:09 +0000

    add switch, route, router and start navbar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import {Home} from './pages/Home';
 import Error from './pages/Error';
 import Motivation from './pages/Motivation';
 import Projects from './pages/Projects';
+import { Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return ( 
-    <> <Home/>
-       <Error/>
-       <Motivation/>
-       <Projects/>
+    <> 
+    <Navbar/>
+   <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/motivation' component={Motivation}/>
+      <Route exact path='/projects' component={Projects}/>
+      <Route exact path='/error' component={Error}/>
+    </Switch>
+
     </>
   );
    

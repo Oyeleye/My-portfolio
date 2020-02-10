d5bfa8f9d1fb1607c57bd45d31b833f75d416506
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-10 13:08:43 +0000

    change pages path in app js

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
    <Switch>
       <Route exact path='/' component={Home}/>
       <Route exact path='/projects' component={Projects}/>
-      <Route exact path='/projects/motivation' component={Resume}/>
-      <Route exact path='/projects/motivation/:good'  component={Motivation}/>
+      <Route exact path='/resume' component={Resume}/>
+      <Route exact path='/motivation'  component={Motivation}/>
       <Route component={Error} />
     </Switch>
 

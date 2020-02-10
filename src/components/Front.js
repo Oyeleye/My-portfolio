362bda71fdacaaa8df1095768c67ae46dea26797
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-10 16:37:26 +0000

    add 2 props and set defaultFront for use in class

--- src/components/Front.js
+++ src/components/Front.js
@@ -3,3 +3,7 @@ import React from 'react';
 export default function Front( {children, front} ) {
     return <header className={front}>{children}</header>;
 }
+
+Front.defaultProps = {
+    front: 'defaultFront'
+};

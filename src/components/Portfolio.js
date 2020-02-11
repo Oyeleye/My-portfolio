6e40453138e7d0810959b00140a65cc055cfe583
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-11 16:58:52 +0000

    create Portfolio component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import Title from './Title';
+import { FaLaptop, FaServer, FaDatabase, FaCloud } from 'react-icons/fa';
+
+export default class Portfolio extends Component {
+   state= {
+       portfolio: [
+           {
+               icon: <FaLaptop/>,
+               title: 'FRONTEND',
+               info: 'HTML5',
+               info1: 'CSS3',
+               info2: 'JavaScript',
+               info3: 'React | Redux'
+           },
+           {
+               icon: <FaServer/>,
+               title: 'SERVER',
+               info: ''
+           },
+           {
+               icon: <FaDatabase/>,
+               title: 'DATABASE',
+               info: 'PostgreSQL',
+               info1: 'MySQL'
+           },
+           {
+               icon: <FaCloud/>,
+               title: 'DEPLOYMENT',
+               info: ''
+           },
+       ]
+   }
+    render() {
+        return (
+            <section className='portfolio'>
+               <Title title='portfolio' />
+                 <div className='portfolio-center'>
+                     {this.state.portfolio.map((item, index)=> {
+                         return (
+                             <article key={index} className='portfolio'>
+                                 <span>{item.icon}</span>
+                                 <h6>{item.title}</h6>
+                                 <p>{item.info}</p>
+                                 <p>{item.info1}</p>
+                                 <p>{item.info2}</p>
+                                 <p>{item.info3}</p>
+                             </article> 
+                         );
+                     })}
+                 </div>
+            </section>
+        );
+    }
+}

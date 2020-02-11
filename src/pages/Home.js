89d87ccf00ba00eba429a04e745067a56fd149f5
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-11 17:02:01 +0000

    import Portfolio component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Front from '../components/Front';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
+import Portfolio from '../components/Portfolio';
+
 
 export const Home = () => {
     return (
-      
-      <Front> 
-         <Banner  title='OYELEYE OYEDAYO' subtitle='Front-end Developer'>
-            <Link to='/projects' className='btn-primary'>See Projects</Link>
-         </Banner>
-      </Front>
-      );
+     <> 
+         <Front> 
+            <Banner  title='OYELEYE OYEDAYO' subtitle='Front-end Developer'>
+               <Link to='/projects' className='btn-primary'>See Projects</Link>
+            </Banner>
+         </Front>
+         <Portfolio/>
+   
+    </>
+    );
+   
 }
 

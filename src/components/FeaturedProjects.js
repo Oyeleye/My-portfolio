53df3c4ac2c0076ecf71563ef026bd5fe460b650
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-28 08:03:02 +0000

    contextAPI, destructuring and lifecycle

--- src/components/FeaturedProjects.js
+++ src/components/FeaturedProjects.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
-import {ProjectContext} from '../Context';
+import React, { Component } from 'react';
+import { ProjectContext } from '../Context';
 import Loading from './Loading';
 import Project from './Project';
 import Title from './Title';
 
 export default class FeaturedProjects extends Component {
    static contextType = ProjectContext;
-       render() {
-           let { loading, featuredProjects : projects } = this.context;
-           projects = projects.map(project => {
-              return <Project key={project.id} project={project}/>
-           })
-                 return (
-                   <>
-                      <section className='featured-projects'>
-                         <Title title='featured projects' />
-                         <div className='featured-projects.center'>
-                            
-                               {loading ? <Loading /> : projects}
-                         </div>
-                      </section>
-                   </>
-                 );   
-       }
+   render() {
+      //const {name, greetings} = this.context;
+     let { loading, featuredProjects : projects } = this.context;
+     projects = projects.map( project => {
+       return <Project key={project.id} project={project} />
+     })
+      return (
+         <section className='featured-projects'>
+            <Title title='featured-projects' />
+            <div className='featured-projects-center'>
+               {loading? <Loading /> : projects}
+            </div>
+         </section>
+      );
+   }
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,43 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import defaultImg from '../images/defaultBcg.jpeg';
-import featuredProjectsImg1 from '../images/details-1.jpeg';
-import featuredProjectsImg2 from '../images/details-2.jpeg';
-import featuredProjectsImg3 from '../images/details-3.jpeg';
-import featuredProjectsImg4 from '../images/details-4.jpeg';
-import Loading from './Loading';
 
-class Project extends Component {
-    s
- render() { 
+export default function Project( { project } ) {
+   const{name, slug, images, price} = project;
+
     return (
-      <>
-        <Loading />
         <article className='project'>
             <div className='img-container'>
-               <Link to='./projects'>
-               <img src={featuredProjectsImg1 || defaultImg} alt='single project' />
-               </Link>
-            </div>
-            <div className='img-container'>
-               <Link to='./projects'>
-               <img src={featuredProjectsImg2 || defaultImg} alt='single project' />
-               </Link>
-            </div>
-            <div className='img-container'>
-               <Link to='./projects'>
-               <img src={featuredProjectsImg3 || defaultImg} alt='single project' />
-               </Link>
-            </div>
-            <div className='img-container'>
-               <Link to='./projects'>
-               <img src={featuredProjectsImg4 || defaultImg} alt='single project' />
-               </Link>
-            </div>
+            <img src={images[0] || defaultImg} alt='single project' />
 
+            </div> 
         </article>
-      </>
     )
-}
-}
-export default Project;
+} 

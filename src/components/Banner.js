a224aee9e74f6abdda3b1c69dda87d22d140925c
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-09 11:27:22 +0100

    Add Aos animation to banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,17 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 function Banner( {children, title, subtitle} ) {
+    useEffect(()=>{
+        Aos.init({duration:2000});
+      },[]);
+
     return (
         <div className='banner'>
-           <h1> {title} </h1>
+           <h1 data-aos='slide-left'> {title} </h1>
            <div />
-           <p>{subtitle}</p>
+           <p data-aos='slide-right'>{subtitle}</p>
            {children}
         </div>
     );

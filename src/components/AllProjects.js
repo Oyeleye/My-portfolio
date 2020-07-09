5bf55c79029ed07d98402db0b0278018fd356a4f
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-09 22:50:45 +0100

    Create AllProjects component for Projects

---
+++ src/components/AllProjects.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import {ProjectContext} from '../Context';
+import Loading from './Loading';
+import Project from './Project';
+import Title from './Title';
+
+export default class FeaturedProjects extends Component {
+   static contextType = ProjectContext;
+       render() {
+           let { loading, featuredProjects : projects } = this.context;
+           projects = projects.map(project => {
+              return <Project key={project.id} project={project} />
+           })
+                 return (
+                   <>
+                      <section className='featured-projects'>
+                         <Title title='All-projects' />
+                         <div className='featured-projects-center'>
+                               {loading ? <Loading /> : projects}
+                         </div>
+                      </section>
+                   </>
+                 );   
+       }
+}

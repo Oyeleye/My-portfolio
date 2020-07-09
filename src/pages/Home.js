fae0ba548e172916066cf49b9a7fd0c5e2dd2276
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-09 10:58:08 +0100

    Add contact component, material-ui icon and Aos animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Front from '../components/Front';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import Portfolio from '../components/Portfolio';
-import FeaturedProjects from '../components/FeaturedProjects';
-//import MyFeaturedProject from '../components/Project';
-
+import MyFeaturedProject from '../components/FeaturedProjects';
+import Contact from '../components/Contact';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 function Home () {
-    return (
+   useEffect(()=>{
+      Aos.init({duration:2000});
+   },[]);
+
+   return (
      <> 
          <Front> 
-            <Banner  title='OYELEYE OYEDAYO' subtitle='Front-end Engineer'>
-               <Link to='/projects' className='btn-primary'>
+            <Banner  data-aos='fade-up' title='OYELEYE OYEDAYO' subtitle='Full-Stack Developer'>
+               <Link to='/projects' data-aos='fade-up' className='btn-primary'>
                   See Projects
                </Link>
+               <h6>You are welcome</h6>
             </Banner>
+            <ArrowUpwardIcon className='scrollup'/>
          </Front>
          <Portfolio/>
-         <FeaturedProjects />
-      
-      
+       <MyFeaturedProject />
+       <Contact />
    
     </>
     );;

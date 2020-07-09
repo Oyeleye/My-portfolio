15adf55cc741f4190cf2a8e015c45ee848d29ab2
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-09 12:03:10 +0100

    Add Aos animation

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Front from '../components/Front';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
+import MyFeaturedProject from '../components/FeaturedProjects';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 export const Projects = () => {
+  useEffect(()=>{
+    Aos.init({duration:2000});
+  },[]);
+
         return (
+      <>     
            <Front front='otherpagesFront'>
-               <Banner title='Project Portfolio'>
-                  <Link to='/' className='btn-primary'>Return Home</Link>
+               <Banner title='My Projects'>
+                  <Link to='/' data-aos='fade-up' className='btn-primary'>Return Home</Link>
                </Banner>
            </Front>
+           <MyFeaturedProject />
+      </>
         );
 }

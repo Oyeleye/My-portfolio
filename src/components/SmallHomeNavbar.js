e782626592d799e1f55136779512281dfda51cf6
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-11 17:31:24 +0100

    Create a separate Navbar component for Home page

---
+++ src/components/SmallHomeNavbar.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import {FaAlignRight} from 'react-icons/fa';
+import CloseIcon from '@material-ui/icons/Close';
+import { makeStyles } from '@material-ui/core/styles';
+import {Link} from 'react-router-dom';
+import logo from '../images/logo.svg';
+import githublogo from '../images/githublogo.png';
+import linkedin from '../images/linkedin.png';
+
+
+
+export default function SmallNavbar() {
+
+
+    const[isOpen, setIsOpen]= React.useState(false);
+
+    const toggleNavLinks = () =>{
+        setIsOpen (isOpen =>!isOpen);
+    } 
+
+
+    return (
+        
+        <nav className='smallnavbar'>
+            <div className='smallnav-center'>
+            <button type='button' 
+                className='smallnav-btn'
+                onClick={toggleNavLinks}
+            >
+                {isOpen ? <CloseIcon className='smallnav-icon'/> : <FaAlignRight className='smallnav-icon'/> }
+            </button>
+                <ul className={isOpen ? 'smallnav-links smallshow-home-nav' : 'smallnav-links'}>
+                    <li>
+                      <Link to='./projects'>Projects</Link>
+                    </li>
+                    <li>
+                      <Link to='./motivation'>Motivation</Link>
+                    </li>
+                    <li>
+                      <Link to='./resume'>Resume</Link>
+                    </li>
+                    <li>
+                      <a href='https://github.com/Oyeleye' >
+                         <img src={githublogo} alt='My Portfolio'/>
+                      </a>   
+                    </li>
+                    <li>
+                      <a href='https://www.linkedin.com/in/oyedayo-oyeleye-a985b9bb/' >
+                         <img src={linkedin} alt='My Portfolio'/>
+                      </a>   
+                    </li>
+                    <li className='smallnav-links2'>
+                      <Link to='./'>En</Link> 
+                      <Link to='./'>Fi</Link>
+                      <Link to='./'>De</Link>
+                    </li>
+                </ul>
+             </div>   
+        </nav>
+        
+    )
+}

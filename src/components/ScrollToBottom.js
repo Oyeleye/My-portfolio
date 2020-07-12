288c892d4510eafb9c99e091af134edc1cb9fa1a
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-12 09:14:45 +0100

    Create ScrollToButtom

---
+++ src/components/ScrollToBottom.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import { useWindowScroll } from 'react-use';
+
+
+
+export default function ScrollToBottom() {
+
+    const scrollToBottom = () => window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior:'smooth'
+     });
+
+    return (
+        <div className='scroll-to-bottom' onClick={scrollToBottom}>
+            <KeyboardArrowDownIcon className='scroll-to-bottom-icon icon' />
+        </div>
+    )
+}

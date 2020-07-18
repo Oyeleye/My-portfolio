db600b14105efdc387c3f5362e7030bc6ac38519
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-18 18:58:24 +0100

    Add arrow icon and EN for lang dropdown

--- src/components/BlackNavbar.js
+++ src/components/BlackNavbar.js
@@ -1,12 +1,40 @@
+
 import React from 'react'
 import AddIcCallIcon from '@material-ui/icons/AddIcCall';
 import {Link} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import LanguageMenuDropdown from '../components/LanguageMenuDropdown';
 
 export default function BlackNavbar() {
+    const useStyles = makeStyles(theme => ({
+        navIcon: {
+            color: '#fff',
+            cursor: 'pointer',
+
+  
+        },
+        contact: {
+            cursor: 'pointer',
+        },
+        language: {
+            marginLeft: '16rem',
+            paddingBottom: '1rem',
+            color:'#fff',
+            cursor: 'pointer',
+        }
+    })); 
+    const classes = useStyles();
+    
     const scrollToBottom = () => window.scrollTo({
         top: document.documentElement.scrollHeight,
         behavior:'smooth'
       });
+      const[isOpen, setIsOpen]= React.useState(false);  
+
+      const toggleNavLinks = () =>{
+          setIsOpen (isOpen =>!isOpen);
+      } 
 
     return (
         <nav className='black-navbar'>
@@ -14,12 +42,23 @@ export default function BlackNavbar() {
                 <div className='black-nav-header' >             
                     <AddIcCallIcon className='black-nav-icon'/>
                 </div>
-                    <ul className='black-nav-links'>
+                    <span className='black-nav-links'>
                         <a href='tel:+353 (83) 141 7085'> +353 (83) 141 7085 </a>
-                        <a onClick={scrollToBottom} title='Click to go to contact'>Contact</a>
-                </ul>
+                        <a onClick={scrollToBottom} className={classes.contact} title='Click to go to contact'>Contact</a>
+                    </span>
+                    <div className='language-nav'>
+                        
+                        <a onClick={toggleNavLinks } className={classes.language} > 
+                            En                           
+                        </a>
+                        <a>
+                            <ArrowLeftIcon className={classes.navIcon} onClick={toggleNavLinks }/>
+                        </a>
+                    </div>
 
-            </div>               
+            </div>
+
+            {isOpen? <LanguageMenuDropdown /> : '' }               
         </nav>
     )
 }

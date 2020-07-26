54e325329f4491b253b20addc224990ae62903ae
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-26 23:19:04 +0100

    Slight change

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,21 +4,19 @@ import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import SmallNavbar from '../components/SmallNavbar';
+import NavbarOtherPages from '../components/NavbarOtherPages';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import {  } from "module";
 
 const useStyles = makeStyles(theme=> ({
     root: {
-        textAlign: 'center',
         marginTop:10,
         display: 'flex',
         margin: '0 auto',
         borderStyle: 'solid',
         borderWidth: 1,
-        width: 400,
-        padding: 15
+        width: "26rem",
     }
 }));
 
@@ -29,7 +27,7 @@ export const Resume = () => {
     const classes = useStyles();
     return (
         <>
-            <SmallNavbar />
+            <NavbarOtherPages/>
             <Hero hero='otherpagesHero'> 
                 <Banner title='My Resume'>
                     <Link to='/' data-aos='fade-up' className='btn-primary'>Return Home</Link>

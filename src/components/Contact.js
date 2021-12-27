42b89281767ead80455d5370e98ead65890f5dd3
unknown <oyeleyeoyedayo@gmail.com>
2021-12-27 12:40:40 +0000

    change fill to please fill in

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -42,7 +42,7 @@ export default function Contact() {
     }
     const MessageSent = () => {
         return(
-            <h2 className={classes.sentMsgStyle}>Fill the required field(s).</h2>
+            <h2 className={classes.sentMsgStyle}>Please, fill in the field(s).</h2>
         )
     }
     const handleSubmitMessage = () => {
